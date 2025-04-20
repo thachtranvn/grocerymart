2fcfecf9af374f377344afc15a366dfc588360ae
Thach Tran <tranngochoangthach@gmail.com>
2025-04-20 16:13:18 +0700

    Product Description

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -192,6 +192,28 @@ window.addEventListener("template-loaded", () => {
   });
 });
 
+window.addEventListener("template-loaded", () => {
+  const tabsSelector = "prod-tab__item";
+  const contentsSelector = "prod-tab__content-item";
+
+  const tabActive = `${tabsSelector}--current`;
+  const contentActive = `${contentsSelector}--current`;
+
+  const tabContainers = $$(".js-tabs");
+  tabContainers.forEach((tabContainer) => {
+    const tabs = tabContainer.querySelectorAll(`.${tabsSelector}`);
+    const contents = tabContainer.querySelectorAll(`.${contentsSelector}`);
+    tabs.forEach((tab, index) => {
+      tab.onclick = () => {
+        tabContainer.querySelector(`.${tabActive}`)?.classList.remove(tabActive);
+        tabContainer.querySelector(`.${contentActive}`)?.classList.remove(contentActive);
+        tab.classList.add(tabActive);
+        contents[index].classList.add(contentActive);
+      };
+    });
+  });
+});
+
 // ========= ARCHIVED =========
 
 // const $ = document.querySelector.bind(document);
